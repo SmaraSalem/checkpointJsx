157acda3a30bcce9a6f93fbb406825c947441dc5
salemsmara <salemsmara@gmail.com>
2022-06-06 13:35:14 +0200

    initialisation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+
+import gomycode from'./gomycode.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div style={{border: 'Solid 1px black',maxWidth:'100vw'}}>
+
+ <h1 className="title red">Salem smara</h1>
+
+ <br />
+
+ <img src={gomycode} alt="ici un image" />
+
+ <br />
+
+ <img className="img" src="/img/devlop.webp" alt="ici une image"/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src="https://youtu.be/olJOZp57FWM"  />
+
+</video>
+</>
   );
 }
 
